571c04f5b1527d37ece0bf67105e1072eadc3f18
Jule Grigat (16821) <jule.grigat@nordakademie.de>
2025-04-09 13:44:57 +0200

    b is fixed

--- B.py
+++ B.py
@@ -1,7 +1,5 @@
 from neo4j import GraphDatabase
-import csv
-import os
-import sys
+
 
 # Connect to Neo4j
 uri = "bolt://localhost:7687"
@@ -16,12 +14,14 @@ def run_query(query, params=None):
 
 # 1. 3 most cited papers per venue
 query= """
-MATCH (p:Paper)-[:PUBLISHED_IN]->(v:Venue)
-    WHERE v.type IN ["conference", "workshop"]
+ MATCH (p:Paper)-[:PUBLISHED_IN]->(v:Venue)
+WHERE v.type IN ["conference", "workshop"]
 
-    WITH v.venue AS venueName, v.year AS year, p
-    ORDER BY p.citationCount DESC
-    WITH venueName, year, COLLECT({title: p.title, citationCount: p.citationCount})[0..3] AS topPapers
+WITH v.venue AS venueName, v.year AS year, p
+ORDER BY p.citationCount DESC
+WITH venueName, year, COLLECT({title: p.title, citationCount: p.citationCount})[0..3] AS topPapers
+RETURN venueName, year, topPapers
+ORDER BY venueName, year;
     """
 run_query(query)
 
@@ -44,15 +44,12 @@ query = """
 WITH [2021, 2022] AS citedYears, 2023 AS citingYear
 
 MATCH (citing:Paper)-[:CITES]->(cited:Paper)-[:PUBLISHED_IN]->(v:Venue)
-WHERE v.type = "journal" 
-  AND cited.year IN citedYears 
-  AND citing.year = citingYear
+WHERE v.type = "journal" AND cited.year IN citedYears AND citing.year = citingYear
+WITH v, citedYears, COUNT(citing) AS totalCitations
 
-WITH v.venue AS journalName, COUNT(citing) AS totalCitations
-
-MATCH (v:Venue)<-[:PUBLISHED_IN]-(p:Paper)
-WHERE v.type = "journal" AND p.year IN citedYears
-WITH journalName, totalCitations, COUNT(p) AS paperCount
+MATCH (v)<-[:PUBLISHED_IN]-(p:Paper)
+WHERE p.year IN citedYears
+WITH v.venue AS journalName, totalCitations, COUNT(p) AS paperCount
 WHERE paperCount > 0
 RETURN journalName, 
        totalCitations * 1.0 / paperCount AS impactFactor,
